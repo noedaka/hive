28c402bb2dde1e30f566bcdfda12753d0c28a411
noedaka <karobka24@gmail.com>
2025-11-23 14:45:07 +0300

    fix: repository interface correction

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -9,6 +9,15 @@ import (
 type Repository interface {
 	CreateUser(ctx context.Context, userCreds model.UserCredentials) (int, error)
 	GetUserIDByCreds(ctx context.Context, creds model.UserCredentials) (int, error)
+	GetLoginByID(ctx context.Context, authorID int) (string, error)
+	CreatePost(ctx context.Context, post model.Post) error
+	GetPosts(ctx context.Context) ([]model.Post, error)
+	GetPost(ctx context.Context, postID int) (*model.Post, error)
+	CreateComment(ctx context.Context, comment model.Comment) error
+	GetComments(ctx context.Context, authorID int, postID int) ([]model.Comment, error)
+	LikePost(ctx context.Context, userID, postID int) error
+	UnlikePost(ctx context.Context, userID, postID int) error
+	CountPostLikes(ctx context.Context, postID int) (int, error)
 }
 
 type Repo struct {
